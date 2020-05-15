08f260cca5e6018aceaffecdb5f4e9e758959e5e
Daniel Bispo <szagot@gmail.com>
2020-05-15 10:05:30 -0300

    Inciando projeto Spring

---
+++ src/main/java/com/zefuinha/spring_ionic_backend/SpringIonicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zefuinha.spring_ionic_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIonicBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zefuinha/spring_ionic_backend/SpringIonicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zefuinha.spring_ionic_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIonicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
